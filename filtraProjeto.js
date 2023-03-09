e0d30d9f4ba5e9cf2567b9f73642dde0b6ec2238
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-09 10:27:36 -0300

    finalizando projeto aplicando a funcionalidade ao botao todos projetos

--- filtraProjeto.js
+++ filtraProjeto.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll(".bttn")
 
 
-
 botoes.forEach(btn => btn.addEventListener('click', filtrarProjetos))
 
 function filtrarProjetos() {
@@ -11,6 +10,21 @@ function filtrarProjetos() {
     let projetosFiltrados = projetos.filter(projeto => projeto.categoria == categoria)
 
     exibirOsProjetosNaTela(projetosFiltrados)
+
 }
 
 
+const botaoAll = document.querySelectorAll(".bt")
+
+
+botaoAll.forEach(btn => btn.addEventListener('click', filtrarTodosProjetos))
+
+function filtrarTodosProjetos() {
+
+    const elementoBt = document.getElementById(this.id)
+    const categoria = elementoBt.value
+    let todosProjetosFiltrados = projetos.filter(projeto => projeto.mostrar == categoria)
+
+    exibirOsProjetosNaTela(todosProjetosFiltrados)
+
+}
